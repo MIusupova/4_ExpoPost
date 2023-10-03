ccefe77eea4e67ba6e1cd24727b405f608f3d171
MIusupova <li.yusupova.01@mail.ru>
2023-10-03 12:08:37 +0200

    поменяла классы

--- js/users.js
+++ js/users.js
@@ -3,51 +3,48 @@ const users = fetch('https://jsonplaceholder.typicode.com/users')
 .then((data) => renderUsers(data.splice(0, 20)))
 
 const renderUsers = (data) => {
-    const cards = document.querySelector('.cards');
+    const card_hol = document.querySelector('.card-hol');
     data.forEach((item) => {
-      cards.append(createUsers(item));
+      card_hol.append(createUsers(item));
     });
   };
 const createUsers = (item) => {
     const card = document.createElement('div')
-    card.classList.add('card w-75');
+    card.classList.add('card', 'w-75');
     card.dataset.id = item.id;
 
-    const body = document.createElement('div')
-    body.classList.add('card-body');
-    card.append(body);
+    const card_body = document.createElement('div')
+    card_body.classList.add('card-body');
+    card.append(card_body);
 
-    const cardName = document.createElement('h5')
-    cardName.classList.add('card-name')
-    body.append(cardName)
-    cardName.innerText = item.name
+    const card_name = document.createElement('h5')
+    card_name.classList.add('card-name')
+    card_body.append(card_name)
+    card_name.innerText = item.name;
 
     const cardUsername = document.createElement('p')
     cardUsername.classList.add('card-username')
-    body.append(cardUsername)
+    card_body.append(cardUsername)
     cardUsername.innerText = item.username
 
     const cardEmail = document.createElement('p')
     cardEmail.classList.add('card-email')
-    body.append(cardEmail)
+    card_body.append(cardEmail)
     cardEmail.innerText = item.email
 
     const cardPhone = document.createElement('p')
     cardPhone.classList.add('card-phone')
-    body.append(cardPhone)
+    card_body.append(cardPhone)
     cardPhone.innerText = item.phone
 
     const cardWebsite = document.createElement('p')
     cardWebsite.classList.add('card-website')
-    body.append(cardWebsite)
+    card_body.append(cardWebsite)
     cardWebsite.innerText = item.website
-
-    body.innerHTML += 
+    card_body.innerHTML += 
     `<a href="#" class="btn btn-dark">Перейти на страницу!</a>`
-    
-    return card
+    return card;
 
- 
 }
 
 //<div class="card w-75">
