8fdd68557b596cd4346aab849211347fc340db94
MIusupova <145875992+MIusupova@users.noreply.github.com>
2023-11-19 09:31:52 +0100

    Update input.js

--- js/input.js
+++ js/input.js
@@ -3,5 +3,5 @@ const searchBtn = document.querySelector('#search')
 const input = document.querySelector('.form-control')
 searchBtn.addEventListener('click', (event) => {
  event.preventDefault();
- window.location.href = window.location.origin + `/Users/arsenarsanakaev/Desktop/my-project/4_ExpoPost/poisk.html?name=${input.value}`;    //href проверить в консоль лог
+ window.location.href = window.location.origin + ` https://miusupova.github.io/4_ExpoPost/Users/arsenarsanakaev/Desktop/my-project/4_ExpoPost/poisk.html?name=${input.value}`;    //href проверить в консоль лог
 })
