dd6a8c27bd17e81b81fff9099613f9f937aefb9e
MIusupova <145875992+MIusupova@users.noreply.github.com>
2023-11-19 10:20:54 +0100

    Update input.js

--- js/input.js
+++ js/input.js
@@ -3,5 +3,5 @@ const searchBtn = document.querySelector('#search')
 const input = document.querySelector('.form-control')
 searchBtn.addEventListener('click', (event) => {
  event.preventDefault();
- window.location.href = window.location.origin + `/4_ExpoPost/pages/poisk.html?name=${input.value}`;    //href проверить в консоль лог
+ window.location.href = window.location.origin + `/Users/arsenarsanakaev/Desktop/my-project/4_ExpoPost/pages/poisk.html?name=${input.value}`;    //href проверить в консоль лог
 })
